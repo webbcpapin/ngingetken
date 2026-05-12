0489296e91915ae638c9ec5ceaf9392cbaf7b206
webbcpapin <webbcpapin@gmail.com>
2026-05-12 13:08:48 +0700

    Connect frontend to Apps Script backend

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 window.NGINGETKEN_CONFIG = {
-  API_URL: "mock",
-  ADMIN_TOKEN: "local-mode"
+  API_URL: "https://script.google.com/macros/s/AKfycbz41NgYO3L773HpwlaoaMxRixgP4bAkhJ6IqKInP4WX1EWOb8m2MYkVLIcVtMG4U2Y72A/exec",
+  ADMIN_TOKEN: "ganti-token-rahasia"
 };
