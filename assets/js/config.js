c9a854fecabdf96518ba477f296e1accce77f97c
agung3956 <agung3956@gmail.com>
2026-07-02 10:10:28 +0700

    Update Apps Script deployment URL

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 window.NGINGETKEN_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbz_1bKZdm_Jr5IRmkv7w8-65xnvxcg6MuAuoSnKTm7E9_RhuhYiz4QCofbKEdSLhKE37g/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbzsGzflLK-49qMGs0TXugC6XKrgCK-tRzKzE-yE9CSDa5rk9mCGxaGPnZBKSxppiEnjtA/exec",
   ADMIN_TOKEN: "ganti-token-rahasia"
 };
