32dc1f2ca457cdc91449060b02b5acf66c7ee424
Agung Hermawan <agung.hermawan@kemenkeu.go.id>
2026-08-05 12:51:54 +0700

    Update Apps Script deployment endpoint

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 window.NGINGETKEN_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzsGzflLK-49qMGs0TXugC6XKrgCK-tRzKzE-yE9CSDa5rk9mCGxaGPnZBKSxppiEnjtA/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxu00aug7UX5K6mhqsQ90iio_GbFaHmqi3PQsRvEm2Dqt4OL8GiScj-wAC3X_6OTAXxfA/exec",
   ADMIN_TOKEN: "ganti-token-rahasia"
 };
