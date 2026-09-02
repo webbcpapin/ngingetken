9da2e9679e8577b743acc788981bbb50b8930fcf
agung3956 <agung3956@gmail.com>
2026-09-02 10:30:14 +0700

    Update Apps Script endpoint

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 window.NGINGETKEN_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxu00aug7UX5K6mhqsQ90iio_GbFaHmqi3PQsRvEm2Dqt4OL8GiScj-wAC3X_6OTAXxfA/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbwXzGlOLA-9EHgsY2tZvRBL2GxDPDtAGklzQETOVcqk2KuYtN6zgoLHV4dbMaBFlSEokQ/exec",
   ADMIN_TOKEN: "ganti-token-rahasia"
 };
